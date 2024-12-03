2244d4549f5b1d2c07f4b4533634997e61707e8b
SHIVANGI009 <shivangivashistha759@gmail.com>
2024-12-03 15:26:44 +0530

    first commit

---
+++ tests/test_operations.py
@@ -0,0 +1,9 @@
+from src.math_operations import mult,div
+def test_mult():
+    assert mult(2,3)==6
+    assert mult(3,4)==12
+    
+def test_div():
+    assert div(4,2)==2
+    assert div(8,2)==4
+    
